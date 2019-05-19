27b87389a906a3dc17b9e3e4ae0b40de10178140
zhezii <1115384864@qq.com>
2019-05-19 22:04:54 +0800

    认识zuul

---
+++ eurerka/service-zuul/src/main/java/org/zhezii/filter/MyFilter.java
@@ -0,0 +1,50 @@
+package org.zhezii.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Zhou Wenzhe
+ * @date 2019/5/19
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String token = request.getParameter("token");
+        if (!StringUtils.isNotEmpty(token)) {
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (Exception e) {
+
+            }
+            return null;
+        }
+        return null;
+    }
+}

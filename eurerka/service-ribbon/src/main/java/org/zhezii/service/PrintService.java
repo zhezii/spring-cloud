611752c5c77948a142211e75d2199cda04e65bcb
zhezii <1115384864@qq.com>
2019-05-19 21:38:00 +0800

    改造ribbon,增加断路器支持

--- eurerka/service-ribbon/src/main/java/org/zhezii/service/PrintService.java
+++ eurerka/service-ribbon/src/main/java/org/zhezii/service/PrintService.java
@@ -1,5 +1,6 @@
 package org.zhezii.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,7 +15,12 @@ public class PrintService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "printError")
     public String pringName(String name) {
         return restTemplate.getForObject("http://EUREKA-CLIENT/printName/" + name, String.class);
+    }
+
+    public String printError(String name) {
+        return "hi " + name + ",sorry,error";
     }
 }

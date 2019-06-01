ec328ca6f8b9da26b963141a90e61a8955be35bf
zhezii <1115384864@qq.com>
2019-06-01 21:18:47 +0800

    路由网关

---
+++ itoken/itoken-zuul/src/main/java/org/zhezii/ItokenZuulApplication.java
@@ -0,0 +1,13 @@
+package org.zhezii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenZuulApplication.class, args);
+    }
+
+}

a06d52529b747c3be70a87396ffbaa4b0e48b076
zhezii <1115384864@qq.com>
2019-06-01 21:21:58 +0800

    服务监控

---
+++ itoken/itoken-admin/src/main/java/org/zhezii/ItokenAdminApplication.java
@@ -0,0 +1,13 @@
+package org.zhezii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenAdminApplication.class, args);
+    }
+
+}

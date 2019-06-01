f1cdbfc0b4e9ffcb8122a7a70446e3c054519131
zhezii <1115384864@qq.com>
2019-06-01 21:19:36 +0800

    链路追踪

---
+++ itoken/itoken-zipkin/src/main/java/org/zhezii/ItokenZipkinApplication.java
@@ -0,0 +1,13 @@
+package org.zhezii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenZipkinApplication.class, args);
+    }
+
+}

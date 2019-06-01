6e7b5fb6d16be84f5bc297f11afb12718b851a15
zhezii <1115384864@qq.com>
2019-06-01 21:20:19 +0800

    服务注册与发现

---
+++ itoken/itoken-eureka/src/main/java/org/zhezii/ItokenEurekaApplication.java
@@ -0,0 +1,13 @@
+package org.zhezii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenEurekaApplication.class, args);
+    }
+
+}

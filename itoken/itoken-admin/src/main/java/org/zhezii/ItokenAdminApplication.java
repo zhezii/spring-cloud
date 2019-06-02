4c612f59cd96f3ac7e15a5440e161d04f90a2353
zhezii <1115384864@qq.com>
2019-06-02 15:15:57 +0800

    完整版的初始化项目，其中分布式配置中心在另外一个仓库

--- itoken/itoken-admin/src/main/java/org/zhezii/ItokenAdminApplication.java
+++ itoken/itoken-admin/src/main/java/org/zhezii/ItokenAdminApplication.java
@@ -1,9 +1,13 @@
 package org.zhezii;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
 public class ItokenAdminApplication {
 
     public static void main(String[] args) {
--- itoken/itoken-zipkin/src/main/java/org/zhezii/ItokenZipkinApplication.java
+++ itoken/itoken-zipkin/src/main/java/org/zhezii/ItokenZipkinApplication.java
@@ -2,8 +2,12 @@ package org.zhezii;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
 public class ItokenZipkinApplication {
 
     public static void main(String[] args) {

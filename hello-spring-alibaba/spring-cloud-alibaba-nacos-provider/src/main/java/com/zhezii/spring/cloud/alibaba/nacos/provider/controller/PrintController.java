6e6489ecfbaefc57156aeb3836b87dc788373172
zhezii <1115384864@qq.com>
2019-06-14 23:15:48 +0800

    修改服务提供者，测试负载均衡

--- hello-spring-alibaba/spring-cloud-alibaba-nacos-provider/src/main/java/com/zhezii/spring/cloud/alibaba/nacos/provider/controller/PrintController.java
+++ hello-spring-alibaba/spring-cloud-alibaba-nacos-provider/src/main/java/com/zhezii/spring/cloud/alibaba/nacos/provider/controller/PrintController.java
@@ -1,5 +1,6 @@
 package com.zhezii.spring.cloud.alibaba.nacos.provider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PrintController {
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("/print/{message}")
     public String printMessage(@PathVariable("message") String message) {
-        return "hello nacos!print: "+message;
+        return "hello nacos!print: "+message+"i am from "+port;
     }
 }

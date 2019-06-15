9ad619095a5e28670ece5b81cacab522ed91e935
zhezii <1115384864@qq.com>
2019-06-15 22:08:05 +0800

    使用Nacos Config做分布式配置中心

--- hello-spring-alibaba/spring-cloud-alibaba-nacos-provider/src/main/java/com/zhezii/spring/cloud/alibaba/nacos/provider/controller/PrintController.java
+++ hello-spring-alibaba/spring-cloud-alibaba-nacos-provider/src/main/java/com/zhezii/spring/cloud/alibaba/nacos/provider/controller/PrintController.java
@@ -1,6 +1,8 @@
 package com.zhezii.spring.cloud.alibaba.nacos.provider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,13 @@ public class PrintController {
     @Value("${server.port}")
     private String port;
 
+    //注入配置的上下文
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
     @GetMapping("/print/{message}")
     public String printMessage(@PathVariable("message") String message) {
-        return "hello nacos!print: "+message+"i am from "+port;
+        return "hello " + message + " i am from " + port + " and my name is " +
+                applicationContext.getEnvironment().getProperty("user.name");
     }
 }

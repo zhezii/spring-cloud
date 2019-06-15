ab9261b4eafe2d7c8b7e3b13bfc2d0b247a9098d
zhezii <1115384864@qq.com>
2019-06-15 22:06:11 +0800

    路由网关Gateway

---
+++ hello-spring-alibaba/spring-cloud-gateway/src/main/java/com/zhezii/spring/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.zhezii.spring.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Zhou Wenzhe
+ * @date 2019/6/15
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
